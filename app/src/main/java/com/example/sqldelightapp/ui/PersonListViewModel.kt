11303e1d337f8eeffb82e23fa967af5fa2786fc6
karankarhale <karanp.karhale@gmail.com>
2025-08-11 20:05:53 +0530

    step 5: create viewModel

---
+++ app/src/main/java/com/example/sqldelightapp/ui/PersonListViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.sqldelightapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.sqldelightapp.data.PersonDataSource
+
+class PersonListViewModel(personDataSource: PersonDataSource): ViewModel() {
+
+}
---
+++ app/src/main/java/com/example/sqldelightapp/ui/PersonListViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.sqldelightapp.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.sqldelightapp.data.PersonDataSource
+
+class PersonListViewModelFactory(private val personDataSource: PersonDataSource): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(PersonListViewModel::class.java)) {
+            return PersonListViewModel(personDataSource) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
+}
