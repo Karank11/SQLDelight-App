10d1a7002d9cf588631d3eac92706050d05da45e
karankarhale <karanp.karhale@gmail.com>
2025-08-12 12:26:16 +0530

    UI : viewModel implementation

--- app/src/main/java/com/example/sqldelightapp/ui/viewmodel/PersonListViewModel.kt
+++ app/src/main/java/com/example/sqldelightapp/ui/viewmodel/PersonListViewModel.kt
@@ -1,8 +1,47 @@
 package com.example.sqldelightapp.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.sqldelightapp.data.PersonDataSource
+import kotlinx.coroutines.launch
 
-class PersonListViewModel(personDataSource: PersonDataSource): ViewModel() {
+class PersonListViewModel(private val dataSource: PersonDataSource): ViewModel() {
+
+    private val _firstName = MutableLiveData<String>()
+    val firstName: LiveData<String> get() = _firstName
+
+    private val _lastName = MutableLiveData<String>()
+    val lastName: LiveData<String> get() = _lastName
+
+    val personList = dataSource.getAllPersons()
+
+    fun onFirstNameChange(inputText: String) {
+        _firstName.value = inputText
+    }
+
+    fun onLastNameChange(inputText: String) {
+        _lastName.value = inputText
+    }
+
+    fun onInsertPersonClick() {
+        if (firstName.value.isNullOrEmpty() || lastName.value.isNullOrEmpty()) {
+            return
+        }
+
+        viewModelScope.launch {
+            dataSource.insertPerson(firstName.value.toString(), lastName.value.toString())
+        }
+        _firstName.value = ""
+        _lastName.value = ""
+    }
+
+    fun onDeletePersonClick(id: Long) {
+        viewModelScope.launch {
+            dataSource.deletePersonById(id)
+        }
+    }
 
 }
